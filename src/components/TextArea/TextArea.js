32a7f573ea66deb5c1ebf7c7d9ed14362185aa02
Akash Munagala <akash.munagala@gmail.com>
2020-12-29 02:24:20 -0800

    Show chars left when typing in bibliography

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 
 class TextArea extends Component {
 
+  MAX_LEN = 500;
+
   constructor(props){
     super(props)
+    this.state = { charsLeft: this.MAX_LEN };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({ charsLeft: this.MAX_LEN - e.target.value.length })
   }
 
 
@@ -13,7 +20,8 @@ class TextArea extends Component {
     return (
       <div>
         <div>{this.props.label}:</div>
-        <textarea className="textArea" maxLength="500"/>
+        <textarea className="textArea" onChange={this.handleChange} maxLength={this.MAX_LEN}/>
+        <p className="textAreaCharsLeft">{this.state.charsLeft} Characters Left</p>
       </div>
     );
   }
