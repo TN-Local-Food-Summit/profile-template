13945f87fa99f593d6c97404e188e2d7102c2f15
Akash Munagala <akash.munagala@gmail.com>
2020-12-29 02:53:05 -0800

    Convert App to CreateAccount component
    So we can easily transfer this onto the eventual final website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 import TextInput from './components/TextInput/TextInput';
 import TextArea from './components/TextArea/TextArea';
@@ -8,45 +8,50 @@ import BinarySelect from './components/BinarySelect/BinarySelect';
 import Button from './components/Button/Button';
 
 
-function App() {
-  return (
-    <div className="form">
-      <h1 className='header'>Sign Up</h1>
-      <form>
-        <div className="formContent">
-          <div className="centerChildren">
-            <TextInput label='First Name' required={true} />
-            <TextInput label='Last Name' required={true} />
-          </div>
-          <div className="centerChildren">
-            <TextInput label='Email'/>
-            <TextInput label='Phone Number'/>
-          </div>
-          <div className="centerBibliography">
-            <TextArea label='Bibliography' />
-          </div>
+class CreateAccount extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="form">
+        <h1 className='header'>Sign Up</h1>
+        <form>
+          <div className="formContent">
+            <div className="centerChildren">
+              <TextInput label='First Name' required={true} />
+              <TextInput label='Last Name' required={true} />
+            </div>
+            <div className="centerChildren">
+              <TextInput label='Email'/>
+              <TextInput label='Phone Number'/>
+            </div>
+            <div className="centerBibliography">
+              <TextArea label='Bibliography' />
+            </div>
+            
+            
+            <label>Dropdown Industry:</label>
+            <DropDownSelect
+            options={[{name: 'Farming', id: 1},{name: 'Restaurant', id: 2},{name: 'Hospitality', id: 3}, {name: 'Education', id:4}, {name: 'Non-Profit Organization', id:5}]}
+            />
+            <label>Interests:</label>
+            <DropDownMultiSelect
+            options={[{name: 'Healthy Living', id: 1},{name: 'Sustainability', id: 2},{name: 'Food Recipes', id: 3}, {name: 'Food Systems', id:4}]}
+            />
+            <BinarySelect label='Share Information to Local Food Community' />
+            <BinarySelect label='Enable Push Notifications' />
           
-          
-          <label>Dropdown Industry:</label>
-          <DropDownSelect
-          options={[{name: 'Farming', id: 1},{name: 'Restaurant', id: 2},{name: 'Hospitality', id: 3}, {name: 'Education', id:4}, {name: 'Non-Profit Organization', id:5}]}
-          />
-          <label>Interests:</label>
-          <DropDownMultiSelect
-          options={[{name: 'Healthy Living', id: 1},{name: 'Sustainability', id: 2},{name: 'Food Recipes', id: 3}, {name: 'Food Systems', id:4}]}
-          />
-          <BinarySelect label='Share Information to Local Food Community' />
-          <BinarySelect label='Enable Push Notifications' />
-        
-          <div className="centerSubmit">
-            <Button/>
+            <div className="centerSubmit">
+              <Button/>
+            </div>
+            
           </div>
-          
-        </div>
-      </form>
-    </div>
-    
+        </form>
+      </div>
   );
+  }
 }
 
-export default App;
+export default CreateAccount;
